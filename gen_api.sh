c09c745207cb04fee9163f50db3cc00f128fc48c
Guillaume Balaine <igosuki@gmail.com>
2022-08-25 12:12:07 +0200

    swagger codegen

---
+++ gen_api.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+LEMONWAY_API_SWAGGER_URL=${LEMONWAY_API_SWAGGER_URL:-"https://sandbox-api.lemonway.fr/mb/myco/dev/directkitrest/v2/swagger"}
+LEMONWAY_AUTH_SWAGGER_URL=${LEMONWAY_AUTH_SWAGGER_URL:-"https://sandbox-api.lemonway.fr/oauth/swagger/v1/swagger.json"}
+
+BASEDIR=$(dirname 0)
+
+java -jar swagger-codegen-cli-3.0.35.jar generate \
+-i $LEMONWAY_API_SWAGGER_URL \
+-l typescript-node -o $BASEDIR/src/api
+
+java -jar swagger-codegen-cli-3.0.35.jar generate \
+-i $LEMONWAY_AUTH_SWAGGER_URL \
+-l typescript-node -o $BASEDIR/src/auth
