c096b1937b4b09e1301e8b3d91dce4aa53b29390
Guillaume Balaine <igosuki@gmail.com>
2022-08-26 10:58:27 +0200

    execute code gen and create index.ts

---
+++ src/api/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountsApi';
+export * from './DisputesApi';
+export * from './MoneyInsApi';
+export * from './MoneyOutsApi';
+export * from './P2PsApi';
+export * from './RefundsApi';
---
+++ src/auth/models/index.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Bearer';
+export * from './ProblemDetails';
